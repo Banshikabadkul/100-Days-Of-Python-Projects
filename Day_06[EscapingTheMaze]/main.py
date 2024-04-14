998e6a77bd74b3b0167b705e57ecdeb9c6061600
Banshika <banshikabadkul@gmail.com>
2024-04-14 21:28:47 +0530

    Day 6  project commit

---
+++ Day_06[EscapingTheMaze]/main.py
@@ -0,0 +1,22 @@
+#  link to the wscaping maze game :"https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json"
+
+#  code is
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left() 
+# while front_is_clear():
+#     move()
+# turn_left()
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+        
+#     else:
+#         turn_left()
+        
